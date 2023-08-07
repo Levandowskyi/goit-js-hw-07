38831bbfe827cf5c334e97d8ebce35fde72c01c0
Levandowskyi <stcorvin@gmail.com>
2023-08-07 10:42:22 +0300

    Gallery update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+function createMarkup(arr) {
+    return arr.map(({description,original,preview}) => 
+    `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" data-source = "${original}" />
+   </a>
+</li>`).join('')
+}
+
+gallery.insertAdjacentHTML('beforeend',createMarkup(galleryItems))
+
+gallery.addEventListener('click', handlerClick)
+
+function handlerClick(evt) {
+    evt.preventDefault()
+    if (!evt.target.nodeName === "IMG" ) {
+        return
+    }
+
+    let gallery = new SimpleLightbox('.gallery a',{captionsData:"alt",captionsDelay:250});
+
+}
